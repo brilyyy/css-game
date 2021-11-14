415e8c7c7bf685f55fb310a628e987f6537e0e56
Brilyan <49208216+brilyyy@users.noreply.github.com>
2021-11-15 01:08:12 +0700

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,7 @@
 </script>
 
 <div class="flex flex-col h-screen justify-center items-center">
-	<h1 class="font-bold text-2xl mb-3">Simple CSS Game</h1>
-	<a href="/play/1">play game</a>
+	<h1 class="font-bold text-2xl mb-1">Simple CSS Game</h1>
+	<h1 class="font-bold mb-3">Didesain oleh Theodora, dibuat oleh brilyyy</h1>
+	<a class="hover:text-blue-400" href="/play/1">play game</a>
 </div>
